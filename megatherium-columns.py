295533cb82d0d618ebe536be2db62fc50d5ab296
jjjjoao00 <108374205+jjjjoao00@users.noreply.github.com>
2026-03-12 17:01:45 -0300

    Add files via upload
    SCRIPT INICIAL PARA A QUANTIFICAÇÃO DE FORMAS PARA PILARES

---
+++ megatherium-columns.py
@@ -0,0 +1,71 @@
+print ("METODOLOGIA DE EXTRAÇÃO DE QUANTITATIVO DE FORMA PARA VIGAS DE CONCRETO ARMADO UTILIZANDO IFCOPENSHELL")
+
+import ifcopenshell
+import ifcopenshell.geom
+import ifcopenshell.util.shape
+import ifcopenshell.util.element
+import ifc5d
+import math
+import csv
+import os 
+import platform
+
+model = ifcopenshell.open('C:/Users/Dell/Desktop/DOCS/EXTRACAO-IFC/modelopilar1.ifc')
+
+#PILARES
+columns = model.by_type('IfcColumn')[0]
+column_type = ifcopenshell.util.element.get_type(columns)
+lado_x = []
+lado_y = []
+lado_z =[]
+total_lado_x = 0
+total_lado_y = 0
+total_lado_z = 0
+total_faces = 0
+
+#INICIALIZAÇÃO AUTOMATICA DO EXCEL
+diretorio_script = os.path.dirname(os.path.abspath(__file__))
+nome_arquivo = 'formas.csv'
+caminho_final = os.path.join(diretorio_script, nome_arquivo)
+
+#CALCULO DOS PILARES DO MODELO
+#SELECIONA O PILAR NO MODELO PELO TIPO IFC 'IfcColumn'
+for pilar in model.by_type ('IfcColumn', 1):
+	#NOMEIA OS PILARES A PARTIR DA ESPECIFICAÇÃO TAG NO PROJETO
+	print(pilar.Tag)
+
+	#RETORNA A FORMA GEOMÉTRICA DOS PILARES NO MODELO
+	settings = ifcopenshell.geom.settings()
+	shape = ifcopenshell.geom.create_shape (settings, pilar)
+
+	#RETORNA O VOLUME DE CADA PILAR
+	volume = round(ifcopenshell.util.shape.get_volume(shape.geometry), 2)
+	print (f'Volume de concreto : {volume} m3')
+
+	#VERIFICAÇÃO DAS DIMENSÕES EM CADA EIXO
+	#RETORNA A DIMENSÃO EM X
+	facex = round(ifcopenshell.util.shape.get_x(shape.geometry), 2)
+	print(f'face x: {facex:.2f}')
+
+	#RETORNA A DIMENSÃO EM Y
+	facey = round(ifcopenshell.util.shape.get_y(shape.geometry),2)
+	print(f'face y: {facey:.2f}')
+
+	#RETORNA A DIMENSÃO EM Z 
+	facez = round (ifcopenshell.util.shape.get_z(shape.geometry), 2)
+	print(f'face z: {facez:.2f}')
+
+	#CALCULA A ÁREA DE SUPERFÍCIE EM DETERMINADO EIXO
+	area_z = round ((ifcopenshell.util.shape.get_side_area(shape.geometry, 'Z', angle = 90.0)),2)
+	print (f'Area Z : {area_z:.2f} m2')
+
+	area_y = round((ifcopenshell.util.shape.get_side_area(shape.geometry, 'Y', angle = 90.0)),2)
+	print (f'Area Y : {area_y:.2f} m2')
+
+	area_x = round((ifcopenshell.util.shape.get_side_area(shape.geometry, 'X', angle = 90.0)),2)
+	print (f'Area X : {area_x:.2f} m2')
+
+
+
+#print (f'Total de pilares no modelo', len(column_type))
+
